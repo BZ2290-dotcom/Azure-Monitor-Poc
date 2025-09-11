d1ad1721eb2cc58f649e8727ff4ca0cefd63debe
BZ2290-dotcom <bassma.zaki@outlook.com>
2025-09-11 18:22:50 +0300

    Update main.tf

--- terraform/log_analytics/main.tf
+++ terraform/log_analytics/main.tf
@@ -1,1 +1,17 @@
+provider "azurerm" {
+  features {}
+}
+
+resource "azurerm_resource_group" "rg" {
+  name     = "poc-monitor-rg"
+  location = "East US"
+}
+
+resource "azurerm_log_analytics_workspace" "law" {
+  name                = "poc-law"
+  location            = azurerm_resource_group.rg.location
+  resource_group_name = azurerm_resource_group.rg.name
+  sku                 = "PerGB2018"
+  retention_in_days   = 30
+}
 
