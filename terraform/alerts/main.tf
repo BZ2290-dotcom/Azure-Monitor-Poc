93733cf666bf85080f2c05b42c9c850f80f2d858
BZ2290-dotcom <bassma.zaki@outlook.com>
2025-09-12 17:19:47 +0300

    Update main.tf

--- terraform/alerts/main.tf
+++ terraform/alerts/main.tf
@@ -1,9 +1,22 @@
+resource "azurerm_monitor_action_group" "logicapp_action_group" {
+  name                = "poc-action-group"
+  resource_group_name = "poc-monitor-rg"
+  short_name          = "POCAG"
+  location            = "Global"
+
+  webhook_receiver {
+    name                    = "LogicAppWebhook"
+    service_uri             = "https://<your-logic-app-trigger-url>"
+    use_common_alert_schema = true
+  }
+}
+
 resource "azurerm_monitor_scheduled_query_rules_alert" "cpu_alert" {
   name                = "poc-cpu-alert"
   location            = "East US"
   resource_group_name = "poc-monitor-rg"
   action {
-    action_group = [azurerm_monitor_action_group.alert_group.id]
+    action_group = [azurerm_monitor_action_group.logicapp_action_group.id]
   }
 
   data_source_id = azurerm_log_analytics_workspace.law.id
