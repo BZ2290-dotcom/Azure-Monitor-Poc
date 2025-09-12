a8902d961c76f90b9b1a17ddd4550a407d21c1b8
BZ2290-dotcom <bassma.zaki@outlook.com>
2025-09-12 11:10:59 +0300

    Update main.tf

--- terraform/alerts/main.tf
+++ terraform/alerts/main.tf
@@ -1,1 +1,28 @@
+resource "azurerm_monitor_scheduled_query_rules_alert" "cpu_alert" {
+  name                = "poc-cpu-alert"
+  location            = "East US"
+  resource_group_name = "poc-monitor-rg"
+  action {
+    action_group = [azurerm_monitor_action_group.alert_group.id]
+  }
+
+  data_source_id = azurerm_log_analytics_workspace.law.id
+  description    = "Alert when CPU usage is high"
+  severity       = 2
+  frequency      = 5
+  time_window    = 10
+  query          = <<QUERY
+Perf
+| where ObjectName == "Processor" and CounterName == "% Processor Time"
+| summarize avg(CounterValue) by bin(TimeGenerated, 5m)
+| where avg_CounterValue > 80
+QUERY
+
+  trigger {
+    operator  = "GreaterThan"
+    threshold = 80
+  }
+
+  enabled = true
+}
 
