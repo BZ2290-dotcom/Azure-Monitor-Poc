be818f6267b4c3e6a41b2c7dd29f1e00b94831ac
BZ2290-dotcom <bassma.zaki@outlook.com>
2025-09-14 22:35:10 +0300

    Update main.tf

--- terraform/dcr/main.tf
+++ terraform/dcr/main.tf
@@ -1,31 +1,42 @@
+provider "azurerm" {
+  features {}
+
+  client_id       = var.client_id
+  client_secret   = var.client_secret
+  subscription_id = var.subscription_id
+  tenant_id       = var.tenant_id
+}
+
+data "azurerm_log_analytics_workspace" "existing" {
+  name                = "poc-law"
+  resource_group_name = "poc-monitor-rg"
+}
+
 resource "azurerm_monitor_data_collection_rule" "dcr" {
   name                = "poc-dcr"
   location            = "East US"
   resource_group_name = "poc-monitor-rg"
 
   data_flow {
-    streams      = ["Microsoft-InsightsMetrics"]
-    destinations = ["logAnalyticsDestination"]
+    streams      = ["Microsoft-Perf"]
+    destinations = ["log_analytics"]
   }
 
   destinations {
     log_analytics {
-      name                  = "logAnalyticsDestination"
-      workspace_resource_id = "/subscriptions/e82f8c84-0b4f-4914-b3fb-44663a8edb99/resourceGroups/poc-monitor-rg/providers/Microsoft.OperationalInsights/workspaces/poc-law"
+      name               = "log_analytics"
+      workspace_resource_id = data.azurerm_log_analytics_workspace.existing.id
     }
   }
 
   data_sources {
     performance_counter {
-      name           = "perfCounterSource"
-      streams        = ["Microsoft-InsightsMetrics"]
+      name = "perf-cpu"
+      streams = ["Microsoft-Perf"]
       sampling_frequency_in_seconds = 60
       counter_specifiers = [
-        "\\Processor(_Total)\\% Processor Time",
-        "\\Memory\\Available MBytes"
+        "\\Processor(_Total)\\% Processor Time"
       ]
     }
   }
-
-  description = "POC DCR for collecting performance metrics"
 }
