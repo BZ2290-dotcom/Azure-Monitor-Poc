5e3033c0013f301dfb584a297da3ae509fa3555b
BZ2290-dotcom <bassma.zaki@outlook.com>
2025-09-14 21:03:17 +0300

    Update variables.tf

--- terraform/vm/variables.tf
+++ terraform/vm/variables.tf
@@ -2,3 +2,18 @@ variable "log_analytics_workspace_id" {
   type        = string
   description = "The ID of the Log Analytics workspace to send diagnostics to"
 } 
+variable "client_id" {
+  type = string
+}
+
+variable "client_secret" {
+  type = string
+}
+
+variable "subscription_id" {
+  type = string
+}
+
+variable "tenant_id" {
+  type = string
+}
