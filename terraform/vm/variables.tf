d857a8ec0dc4727db943d8937348dbedf940fca1
BZ2290-dotcom <bassma.zaki@outlook.com>
2025-09-14 21:56:15 +0300

    Update variables.tf

--- terraform/vm/variables.tf
+++ terraform/vm/variables.tf
@@ -1,7 +1,3 @@
-variable "log_analytics_workspace_id" {
-  type        = string
-  description = "The ID of the Log Analytics workspace to send diagnostics to"
-} 
 variable "client_id" {
   type = string
 }
