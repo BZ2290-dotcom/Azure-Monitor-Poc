1cbffe09637c54a56a2004f958d21c5c5ba9acb4
BZ2290-dotcom <bassma.zaki@outlook.com>
2025-09-14 23:09:37 +0300

    Update variables.tf

--- terraform/dcr_association/variables.tf
+++ terraform/dcr_association/variables.tf
@@ -1,3 +1,20 @@
-variable "vm_id" {
+variable "client_id" {
   type = string
+}
+
+variable "client_secret" {
+  type = string
+}
+
+variable "subscription_id" {
+  type = string
+}
+
+variable "tenant_id" {
+  type = string
+}
+
+variable "vm_id" {
+  type        = string
+  description = "The resource ID of the VM to associate with the DCR"
 }
