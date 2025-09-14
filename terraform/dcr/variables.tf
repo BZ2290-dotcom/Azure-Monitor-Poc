7d14990a6d4cd69d1e5d7e2792812cc8e9745d62
BZ2290-dotcom <bassma.zaki@outlook.com>
2025-09-14 22:36:07 +0300

    Update variables.tf

--- terraform/dcr/variables.tf
+++ terraform/dcr/variables.tf
@@ -1,3 +1,15 @@
-variable "location" {
-  default = "East US"
+variable "client_id" {
+  type = string
+}
+
+variable "client_secret" {
+  type = string
+}
+
+variable "subscription_id" {
+  type = string
+}
+
+variable "tenant_id" {
+  type = string
 }
